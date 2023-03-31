40d3f80b0c9c6e7db73b70309f6aa1ae61c83b80
Magic Len <len@magiclen.org>
2023-03-31 09:06:05 +0800

    update doc

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,15 @@ This crate provides the `assert_eq_float!` macros that support floats.
 ## Examples
 
 ```rust
-use assert_eq_float::assert_eq_float;
+use assert_eq_float::*;
 
-assert_eq_float!(1.1 + 0.1, 1.2); // error = 0.0000000000000021316282072803005
+assert_eq_float!(1.1 + 0.1, 1.2);       // error = 0.0000000000000021316282072803005
 assert_eq_float!(1e100 + 2e100, 3e100); // error = 53290705182007510000000000000000000000000000000000000000000000000000000000000000000000
+
+// other macros
+debug_assert_eq_float!(0.0, 0.0);
+assert_ne_float!(0.0, 0.1);
+debug_assert_ne_float!(0.0, 0.1);
 ```
 
 The default margin of error is dynamically computed by properties of IEEE 754 floating point numbers. You don't need to worry about it if you just want to check two float values are **equal**.
